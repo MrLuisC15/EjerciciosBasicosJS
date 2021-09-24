d6288f26e71a81d55e676c22aba31bccc936af57
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-09-24 12:15:49 +0200

    Comment

--- Ejercicio10/ejercicio10.js
+++ Ejercicio10/ejercicio10.js
@@ -2,7 +2,7 @@
 
 let frase = prompt('Canta por esa boquita...')
 
-document.write(frase+"<br />")
+// document.write(frase+"<br />")
 
 for(let i=0;i<frase.length();i++) {
     if(frase.toLowerCase=='a' || frase.toLowerCase=='e' || frase.toLowerCase=='i' || frase.toLowerCase=='o' || frase.toLowerCase=='u')
