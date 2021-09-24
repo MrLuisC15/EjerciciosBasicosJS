b9598162aa376de68653ea5591550a2914c50de5
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-09-24 12:18:57 +0200

    arreglado

--- Ejercicio10/ejercicio10.js
+++ Ejercicio10/ejercicio10.js
@@ -4,7 +4,7 @@ let frase = prompt('Canta por esa boquita...')
 
 // document.write(frase+"<br />")
 
-for(let i=0;i<frase.length();i++) {
+for(let i=0;i<frase.length;i++) {
     if(frase.charAt(i).toLowerCase=='a' || frase.charAt(i).toLowerCase=='e' || frase.charAt(i).toLowerCase=='i' || frase.charAt(i).toLowerCase=='o' || frase.charAt(i).toLowerCase=='u')
         document.write(frase.charAt(i))
 }
